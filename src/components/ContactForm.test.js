527e2d44b648dae7de23417a6f2645f0f4e2bc45
Gabriel Henton <gabehenton@gmail.com>
2021-01-21 15:48:05 -0800

    not all tests are passing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,39 +1,39 @@
-import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
-test('renders ContactForm w/o errors', () => {
-    render(< ContactForm />);
+test('renders ContactForm', () => {
+  render(<ContactForm />);
 });
-test('User can fill out and submit form', async () => {
+test('User can fill out and submit form', () => {
+  render(<ContactForm />);
+  // create variables that allow us to test our input fields
+  const firstName = screen.getByText(/first name/i);
+  const lastName = screen.getByText(/last name/i);
+  const emailAddrs = screen.getByText(/email/i);
+  const msg = screen.getByText(/message/i);
 
-    render(<ContactForm />);
+  // enter text in the fields
 
-    const firstName = await screen.getByText(/first name/i);
-    const lastName = await screen.getByText(/last name/i);
-    const emailAdrs = await screen.getByText(/email/i);
-    const msg = await screen.getByText(/message/i);
+  userEvent.type(firstName, 'Gabriel');
+  userEvent.type(lastName, 'Henton');
+  userEvent.type(emailAddrs, 'g@g.com');
+  userEvent.type(msg, 'Success!');
 
-//lets us enter text in the fields
-    fireEvent.change(firstName, {
-        target: { name: 'Gabriel'}});
+  // test expectations
+  expect(firstName).toHaveValue('Gabriel');
+  expect(lastName).toHaveValue('Henton');
+  expect(msg).toHaveValue('Success!');
+  expect(emailAddrs).toHaveValue('g@g.com');
 
-    fireEvent.change(lastName, {
-        target: { name: 'Henton' }});
+  //submit button test
+  const button = screen.getByRole('button');
+  userEvent.click(button);
 
-    fireEvent.change(emailAdrs, {
-        target: { name: 'gabrielhenton@gmail.com' }});
+  const newContact = screen.findByText(/Gabriel Henton/i);
 
-    fireEvent.change(msg, {
-        target: { name: 'This is a test' }});
-
-    //submit button test
-    const button = screen.getByRole('button');
-    fireEvent.click(button);
-
-    await screen.queryByText(/gabriel/i);
-    await screen.queryByText(/henton/i);
-    await screen.queryByText(/gabrielhenton@gmail.com/i);
-    await screen.queryByText(/this is a test/i);
-
+  newContact.then((item) => {
+    expect(item).toBeTruthy();
+  });
 });
